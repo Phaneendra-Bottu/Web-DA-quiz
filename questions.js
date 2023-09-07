b4eefa33e2d7a0dd35735cd5ebf46790ab972242
Phaneendra Bottu <bottuphaneendra@gmail.com>
2023-09-07 23:25:44 +0530

    ques

--- questions.js
+++ questions.js
@@ -55,18 +55,176 @@ let questions = [
     "Varisu"
   ]
 },
+{
+  numb: 6,
+  question: "Guess this Telugu Movie from 2020: 🌪 🧚‍♀ 🤖 🏞 ✨",
+  answer: "Bheeshma",
+  options: [
+    "Bheeshma",
+    "Vakeel Saab",
+    "Ala Vaikunthapurramuloo",
+    "Sarileru Neekevvaru"
+  ]
+  },
+
+  {
+    numb: 7,
+  question: "Guess this Telugu Movie from 2018: 🏹 🏰 🌟 🔪 💔",
+  answer: "Aravinda Sametha Veera Raghava",
+  options: [
+    "Rangasthalam",
+    "Jersey",
+    "Aravinda Sametha Veera Raghava",
+    "Mahanati"
+  ]
+  },
+
+  {
+    numb: 8,
+  question: "Guess this Telugu Movie from 2020: 🍽 🍚 🎶 🥁 💕",
+  answer: "Middle Class Melodies",
+  options: [
+    "Middle Class Melodies",
+    "Krishna and His Leela",
+    "Colour Photo",
+    "Uma Maheswara Ugra Roopasya"
+  ]
+  },
+  {
+  numb: 9,
+  question: "Guess this Movie: 🦁 👑 🐺 🌳",
+  answer: "Lion King",
+  options: [
+    "Simha",
+    "Lion King",
+    "The Forest",
+    "Varisu"
+  ]
+},  
+{
+  numb: 10,
+  question: "Guess this Movie: 🐍 🦍 🐅 👦🏽 🌳 📖",
+  answer: "Jungle Book",
+  options: [
+    "Tiger Nageshwar Rao",
+    "Jumanji",
+    "Jungle Book",
+    "NagaValli"
+  ]
+},
+{
+  numb: 11,
+  question: "Guess this Movie: 🚢 👩🏻‍❤️‍👨🏻 🌊 💥 💀",
+  answer: "Titanic",
+  options: [
+    "Veera Shima Reddy",
+    "Uppena",
+    "Happy Holiday",
+    "Titanic"
+  ]
+},
+  {
+  numb: 12,
+  question: "Guess this Movie: 👨🏾 🌊 🚣🏽‍♂️ 🐅",
+  answer: "Life of Pie",
+  options: [
+    "Noah",
+    "Life of Pie",
+    "RRR",
+    "Liger"
+  ]
+},
+  {
+  numb: 13,
+  question: "Guess this Movie: 🐍 🦍 🐅 👦🏽 🌳 📖",
+  answer: "Jungle Book",
+  options: [
+    "Tiger Nageshwar Rao",
+    "Jumanji",
+    "Jungle Book",
+    "NagaValli"
+  ]
+},
+  {
+  numb: 14,
+  question: "Guess this Movie: 🔎 🐟 ",
+  answer: "Finding Nemo",
+  options: [
+    "Finding Nemo",
+    "The Search",
+    "Aqua Man",
+    "Life of Fish"
+  ]
+},
+  {
+  numb: 15,
+  question: "Guess this Movie: 🦁 👑 🐺 🌳",
+  answer: "Lion King",
+  options: [
+    "Simha",
+    "Lion King",
+    "The Forest",
+    "Varisu"
+  ]
+},  {
+  numb: 16,
+  question: "Guess this Movie: 🚢 👩🏻‍❤️‍👨🏻 🌊 💥 💀",
+  answer: "Titanic",
+  options: [
+    "Veera Shima Reddy",
+    "Uppena",
+    "Happy Holiday",
+    "Titanic"
+  ]
+},
+  {
+  numb: 17,
+  question: "Guess this Movie: 👨🏾 🌊 🚣🏽‍♂️ 🐅",
+  answer: "Life of Pie",
+  options: [
+    "Noah",
+    "Life of Pie",
+    "RRR",
+    "Liger"
+  ]
+},
+  {
+  numb: 18,
+  question: "Guess this Telugu Movie from 2018: 🕵‍♂ 📁 🕶 💼 🤐",
+  answer: "Goodachari",
+  options: [
+    "KGF: Chapter 1",
+    "Aravinda Sametha Veera Raghava",
+    "Goodachari",
+    "Mahanati"
+  ]
+},
+  {
+  numb: 19,
+  question: "Guess this Movie: 🔎 🐟 ",
+  answer: "Finding Nemo",
+  options: [
+    "Finding Nemo",
+    "The Search",
+    "Aqua Man",
+    "Life of Fish"
+  ]
+},
+  {
+  numb: 20,
+  question: "Guess this Telugu Movie from 2019: 🌾 🏏 🚴‍♂ 🎖 🥇",
+  answer: "Jersey",
+  options: [
+    "Rangasthalam",
+    "Jersey",
+    "Mahanati",
+    "Arjun Reddy"
+  ]
+},
+]
+
+/*
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-];
+  
+*/
